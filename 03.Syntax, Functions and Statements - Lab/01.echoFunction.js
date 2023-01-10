723ee4da9ee9c28c7027d9eb1b807b2341559600
AbyssKnight7aken <7akenbg@gmail.com>
2023-01-10 22:05:25 +0200

    New solved problem!

---
+++ 03.Syntax, Functions and Statements - Lab/01.echoFunction.js
@@ -0,0 +1,8 @@
+function echoFunction(InputString) {
+
+    console.log(InputString.length);
+    console.log(InputString);
+}
+
+echoFunction('Hello, JavaScript!')
+echoFunction('strings are easy')
