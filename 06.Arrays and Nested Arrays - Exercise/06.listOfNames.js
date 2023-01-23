74fca5dcbbc714848cb01e89856684ddd43b1da4
AbyssKnight7aken <7akenbg@gmail.com>
2023-01-23 23:54:00 +0200

    New solved problem!

---
+++ 06.Arrays and Nested Arrays - Exercise/06.listOfNames.js
@@ -0,0 +1,10 @@
+function listOfNames(array) {
+
+    const result = array.sort((a, b) => a.localeCompare(b))
+        .forEach((element, i) => {
+            console.log(`${i + 1}.${element}`);
+        });
+
+}
+
+listOfNames(["John", "Bob", "Christina", "Ema"])
